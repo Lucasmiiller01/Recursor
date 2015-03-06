480f60d873f0e3e2b4d1bf23080f093b8dd7ef81
Lucasmiillers2 <lucasmiiller00007@hotmail.com>
2015-03-06 11:19:36 -0300

    Trabalho
    Trabalho

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Recursor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.Caixa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Interativa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Recursiva";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(9, 149);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(13, 13);
+            this.Result.TabIndex = 2;
+            this.Result.Text = "0";
+            // 
+            // Caixa
+            // 
+            this.Caixa.Location = new System.Drawing.Point(42, 1);
+            this.Caixa.Name = "Caixa";
+            this.Caixa.Size = new System.Drawing.Size(193, 20);
+            this.Caixa.TabIndex = 3;
+            this.Caixa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Caixa_KeyPress);
+            // 
+            // Recursor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Caixa);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Recursor";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox Caixa;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Recursor : Form
+    {
+        private double number;
+        private double total;
+       
+
+       // private double total;
+        public Recursor()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button Clicou = sender as Button;
+            string Caixa_t = Caixa.Text;
+            total = 1;
+            number = Convert.ToDouble(Caixa_t);
+            for (Double i = number; i > 0; i--)
+            {
+                total *= i;
+                
+                
+            }
+            Result.Text = (total).ToString(); 
+      
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button Clicou = sender as Button;
+            
+            number = Convert.ToDouble(Caixa.Text);
+            total = contar(number);
+            Result.Text = (total).ToString(); 
+        }
+         double contar (double batata)
+        {
+            if(batata < 1) return batata = 1;
+                return contar(batata - 1) * batata;
+        }
+
+         private void Caixa_KeyPress(object sender, KeyPressEventArgs e)
+         {
+             if (e.KeyChar == (char)Keys.Space)
+             {
+                 e.Handled = true;
+             }
+             else
+             {
+                 if (e.KeyChar.ToString().IndexOfAny(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }) == -1)
+                 {
+                     e.Handled = true;
+                 }
+             }
+
+         }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+
+         }
+            
+            
+       
+    }
+}
